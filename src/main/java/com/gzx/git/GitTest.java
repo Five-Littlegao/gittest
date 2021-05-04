f7305f0adb4b4550c27e7ce2394f3888471b78f9
paidaxing <gzzz4999@163.com>
2021-05-04 10:44:10 +0800

    second commit

--- src/main/java/com/gzx/git/GitTest.java
+++ src/main/java/com/gzx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.gzx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
